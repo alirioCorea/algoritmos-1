7234ba6c96ec36da651a78f2fd3f89d81b050ac7
Alirio Corea <coreaalirio@gmail.com>
2023-02-09 16:59:05 -0600

    Fixed some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import Formulario from "./components/Formulario"
-import Respuesta from "./components/Respuesta"
-import Imagen from "./assets/img/instructor.svg"
-import { useState } from "react"
+import Formulario from "./components/Formulario";
+import Respuesta from "./components/Respuesta";
+import Imagen from "./assets/img/instructor.svg";
+import { useState } from "react";
 
 function App() {
   const [div, setDiv] = useState(0);
   const [mod, setMod] = useState(0);
   return (
-    <div className="h-screen">
-      <div className="h-2/4">
-        <div className="flex justify-center h-4/5">
-          <img src={Imagen} alt="teacher"/>
+    <div>
+      <div className="flex flex-col">
+        <div className="grow flex justify-center">
+          <div className="sm:w-6/12 md:w-3/12">
+            <img src={Imagen} alt="Imagen" className="w-full" />
+          </div>
         </div>
-        <div className="flex justify-center">
-          <h1 className="font-black text-3xl text-custom-text">Calculadora para el <span className="text-custom-button">DIV</span> y <span className="text-custom-button">MOD</span></h1>
+        <div className="grow-0 pb-3">
+          <h1 className="font-black text-3xl text-custom-text text-center">
+            Calculadora para el <span className="text-custom-button">DIV</span>{" "}
+            y <span className="text-custom-button">MOD</span>
+          </h1>
         </div>
       </div>
-      <div className="h-2/4">
-        <div className="md:flex h-full">
-          <div className="md:w-3/6 md:h-full">
-          <Formulario
-            setDiv={setDiv}
-            setMod={setMod}
-          />
+      <div>
+        <div className="md:flex">
+          <div className="md:w-3/6 mb-5">
+            <Formulario setDiv={setDiv} setMod={setMod} />
           </div>
-          <div className="md:w-3/6">
-          <Respuesta
-            div={div}
-            mod={mod}
-          />
+          <div className="md:w-3/6 mb-5">
+            <Respuesta div={div} mod={mod} />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
